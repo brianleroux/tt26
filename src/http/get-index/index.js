9d19e53342e0e8e8feca356c3568a4b9987bdf54
Brian LeRoux <b@brian.io>
2021-02-11 11:50:53 -0800

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -17,8 +17,7 @@ let body = `
   <body>
 
     <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
+⭐️ hiiii jackie!
     </h1>
 
     <p class="center-text">
